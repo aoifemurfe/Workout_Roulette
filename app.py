50c2f02aa812a4a963845c6aea1d67f8c0c6c589
aoifemurfe <aoifemurfe@gmail.com>
2020-12-06 15:55:42 +0000

    add mongodb

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 import os
-
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 if os.path.exists("env.py"):
     import env
 
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+
+mongo = PyMongo(app)
+
 
 @app.route("/")
-def hello():
-    return "Hello World ...again!"
+@app.route("/get_birthdays")
+def get_birthdays():
+    users = mongo.db.users.find()
+    return render_template("birthdays.html", users=users)
 
 
 if __name__ == "__main__":
