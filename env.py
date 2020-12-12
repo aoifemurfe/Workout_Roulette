cba6003aeb74165887ee7264a1b0cb6584b7ed28
aoifemurfe <aoifemurfe@gmail.com>
2020-12-12 11:23:17 +0000

    add env file

---
+++ env.py
@@ -0,0 +1,7 @@
+import os
+
+os.environ.setdefault("IP", "0.0.0.0")
+os.environ.setdefault("PORT", "5000")
+os.environ.setdefault("SECRET_KEY", "RYDJ|!%|.{.0vF[7/%vrPEpaM.c!+#")
+os.environ.setdefault("MONGO_URI", "mongodb+srv://aoife:Monday12@cluster0.jjwkr.mongodb.net/birthday_manager?retryWrites=true&w=majority")
+os.environ.setdefault("MONGO_DBNAME", "birthday_manager")
