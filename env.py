5b6d1d99fc490091cfa66684f91c8af6a94024a8
aoifemurfe <52926779+aoifemurfe@users.noreply.github.com>
2021-01-06 20:02:29 +0000

    Delete env.py

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5000")
-os.environ.setdefault("SECRET_KEY", "RYDJ|!%|.{.0vF[7/%vrPEpaM.c!+#")
-os.environ.setdefault("MONGO_URI", "mongodb+srv://aoife:Monday12@cluster0.jjwkr.mongodb.net/workout_roulette?retryWrites=true&w=majority")
-os.environ.setdefault("MONGO_DBNAME", "workout_roulette")
